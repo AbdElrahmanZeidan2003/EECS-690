bcc2f8ace44a7a9125443a4627b7d5e6c8d90828
AbdElrahmanZeidan2003 <130861647+AbdElrahmanZeidan2003@users.noreply.github.com>
2025-03-27 12:42:45 -0500

    Update launch.py

--- Sumo/launch/launch.py
+++ Sumo/launch/launch.py
@@ -4,13 +4,13 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='sumo',
+            package='class_pkg',
             executable='orange',
             name='orange',
             output='screen'
         ),
         Node(
-            package='sumo',
+            package='class_pkg',
             executable='safety_node',
             name='safety_node',
             output='screen'
